79aa49bd20344cd26f30467c74963cff2e1914b2
SARAAG <saraag0036@gmail.com>
2023-11-12 16:24:02 +0530

    Initial Commit

---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Oracle on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
